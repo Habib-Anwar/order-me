2ab8bf0f83b3776898725ec9fdf7b7b94d93dd15
unknown <tauft@aol.com>
2024-03-03 06:41:06 +0600

    project setup

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+        <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <form className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" className="input input-bordered" required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
+    </div>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
---
+++ src/Pages/Shared/ListItem/ListItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const ListItem = ({ children, NavLink }) => {
+  return (
+    <>
+      <li>
+        <a
+          href={NavLink}
+          className="flex py-2 text-base font-medium text-body-color hover:text-dark dark:text-dark-6 dark:hover:text-white lg:ml-12 lg:inline-flex"
+        >
+          {children}
+        </a>
+      </li>
+    </>
+  )
+}
